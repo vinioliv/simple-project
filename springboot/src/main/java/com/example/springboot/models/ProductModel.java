351f5f17a6499538a0b2b8c91f429959162cdffe
Vinicius Oliveira <58372975+vinioliv@users.noreply.github.com>
2023-11-15 12:56:11 -0300

    feat: product model

---
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,25 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+@Getter
+@Setter
+public class ProductModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+}
