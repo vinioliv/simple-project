9c05f20a1585831e54e874946a1094c5d348b6a4
Vinicius Oliveira <58372975+vinioliv@users.noreply.github.com>
2023-11-16 16:53:55 -0300

    feat: list products

--- springboot/src/main/java/com/example/springboot/controllers/ProductController.java
+++ springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -8,23 +8,27 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
 
-    @PostMapping("/products")
+    @PostMapping("/save")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
+    @GetMapping("/list")
+    public ResponseEntity<List<ProductModel>> listProduct(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 }
